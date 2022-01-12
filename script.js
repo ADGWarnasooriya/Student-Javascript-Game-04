f565c8593b4db4283a6a7f1667a8b06caaa1695e
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-01-12 22:46:05 +0530

    function run

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function start() {
     setInterval(run, 200);
 }
+
+var x = 0;
+
+function run() {
+    x = x - 512;
+    var r = document.getElementById("r");
+    r.style.backgroundPositionX = x + "px";
+}
