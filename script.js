5aef06fe31c3ba160cb336cd9422bde40a0f04d2
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-01-12 22:57:54 +0530

    background image move

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ function start() {
 }
 
 var x = 0;
+var i = 0;
 
 function run() {
     x = x - 512;
     var r = document.getElementById("c");
     r.style.backgroundPositionX = x + "px";
+
+    i = i - 10;
+    var d = document.getElementById("d");
+    d.style.backgroundPositionX = i + "px";
 }
